a50677c7780bfb2364436f515bffe8e3ce9ef71c
Javier Mendoza <javier.ing.teleco@gmail.com>
2019-09-23 07:03:11 -0500

    set header & footer

--- README.md
+++ README.md
@@ -7,4 +7,18 @@ http://react-redux-firebase.com/
 
 https://github.com/prescottprue/react-redux-firebase
 
+
+# Depliegue del proyecto en desarrollo 
+
+npm run build:dev
+
+# Depliegue del proyecto en producción 
+
+npm run build:prod
+
+# Release on github & firebase
+
+npm deploy
+
+
 # Actividades pendientes
--- src/app/global/components/layout/Footer/Footer.js
+++ src/app/global/components/layout/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaFacebookSquare, FaGithub, FaTwitter,
-  FaLinkedin, FaYoutube, FaInstagram, FaHeart } from 'react-icons/fa';
+  FaLinkedin, FaYoutube, FaInstagram, FaHeart, FaFacebook } from 'react-icons/fa';
 import { Row,Col, Input, Button } from 'reactstrap';
 
 
@@ -13,12 +13,14 @@ const Footer = () => {
     //className="App-footer"
     <footer className="footer" >
       <Row>
-        <Col>
-          He creado este CMS con ReactJS + Redux & <FaHeart style={{color:'red'}}/>
+        <Col >
+          <strong>
+            He creado este CMS con ReactJS + Redux + <FaHeart style={{color:'red'}}/>
+          </strong>
         </Col>
       </Row>
       <Row>
-        <Col> Suscribete para nuevas publicaciones </Col>
+        <Col className='subcribe'> Suscribete para nuevas publicaciones </Col>
       </Row>
       <Row className='justify-content-center'>
         <Col className='col-auto'>
@@ -29,15 +31,41 @@ const Footer = () => {
         </Col>
       </Row>
       <Row>
-        <Col> <FaFacebookSquare/>&nbsp; Facebook </Col>
-        <Col> <FaGithub/>&nbsp; GitHub </Col>
-        <Col> <FaTwitter/>&nbsp; Twitter </Col>
-        <Col> <FaLinkedin/>&nbsp; LinkedIn </Col>
-        <Col> <FaYoutube/>&nbsp; Youtube </Col>
-        <Col> <FaInstagram/>&nbsp; Instagram </Col>
+        <Col>
+          <a href='https://github.com/Javiermendozain' target="blank" >
+            <FaGithub/>&nbsp; GitHub
+          </a>
+        </Col>
+        <Col>
+          <a href='https://www.linkedin.com/in/javier-eduardo-mendoza-castillo/' target="blank" >
+            <FaLinkedin/>&nbsp; LinkedIn
+          </a>
+        </Col>
+        <Col>
+          <a href='https://www.youtube.com/Javiermendozain' target="blank" >
+            <FaYoutube/>&nbsp; Youtube
+          </a>
+        </Col>
+        <Col>
+          <a href='https://twitter.com/javiermendozain' target="blank" >
+            <FaTwitter/>&nbsp; Twitter
+          </a>
+        </Col>
+        <Col>
+          <a href='https://www.instagram.com/javiermendozain/' target="blank" >
+            <FaInstagram/>&nbsp; Instagram
+          </a>
+        </Col>
+        <Col>
+          <a href='https://www.facebook.com/javiermendozain' target="blank" >
+            <FaFacebookSquare/>&nbsp; Facebook
+          </a>
+        </Col>
       </Row>
+      <small>
+        &copy; {new Date().getFullYear()} Javier Mendoza. All rights reserved.
 
-      &copy; {new Date().getFullYear()} Javier Mendoza. All rights reserved.
+      </small>
     </footer>
   );
 };
